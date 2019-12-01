fefb2aa32a141f926eea6bd37b4361addd728394
Rishi Ravikumar <rishiravi.k98@gmail.com>
2019-12-01 23:40:19 +0530

    fix Kmeans Clustering simulation

--- src/probcalc/KMeansClustering/simMatch.py
+++ src/probcalc/KMeansClustering/simMatch.py
@@ -1,5 +1,5 @@
 import random
-from clusteringProb import *
+
 from CumulativeProb import CP
 from clusteringProb import getKMeansClusters
 import math
@@ -13,62 +13,158 @@ def CalculateRuns(r, batsman_1, bowler, cumulative_prob):
     return run
 
 
-bowlerOrder = ["AB Dinda", "BA Stokes", "AB Dinda", "Imran Tahir", "A Zampa", "BA Stokes", "AB Dinda", "R Bhatia", "DL Chahar", "BA Stokes", "AB Dinda", "Imran Tahir", "A Zampa", "BA Stokes", "AB Dinda", "R Bhatia", "DL Chahar", "BA Stokes", "DL Chahar", "A Zampa"]
-batsmanOrder = ["PA Patel", "JC Buttler", "RG Sharma", "N Rana", "AT Rayudu", "KH Pandya", "KA Pollard", "HH Pandya", "TG Southee", "MJ McClenaghan", "JJ Bumrah"]
+def Innings1():
+	'''bowlerOrder = ["Q de Kock","UT Yadav","V Kohli","AB de Villiers","P Negi","UT Yadav","CJ Anderson","C de Grandhomme","Mohammed Siraj","Mandeep Singh","C de Grandhomme","P Negi","V Kohli","CJ Anderson","Washington Sundar","UT Yadav","Mohammed Siraj","YS Chahal","Q de Kock","P Negi"]
+	batsmanOrder = ["SR Watson","AT Rayudu","SK Raina","SW Billings","RA Jadeja","MS Dhoni","DJ Bravo","Harbhajan Singh","DL Chahar","SN Thakur","Imran Tahir"]'''
+	bowlerOrder = []
+	batsmanOrder = []
 
-# data = open("batsbowlorder.csv", "r")
+	data = open("Team1bats.csv", "r")
+	for line in data.readlines():
+    		batsmanOrder.append(line)
+	
 
-# for line in data.readlines():
-#     line = line.split("\t")
-#     batsmanOrder.append(line[0])
-#     bowlerOrder.append(line[1])
+	data = open("Team2bowl.csv", "r")
+	for line in data.readlines():
+    		bowlerOrder.append(line)
+	
 
-cumulative_prob = CP()
-clustering_prob = getKMeansClusters()
+	cumulative_prob = CP()
+	clustering_prob = getKMeansClusters()
 
-numberOfBalls = 0
-wickets = 0
-score = 0
-ball_score = 0
-overs = 0
-b = 0                                   # Ball in over
+	numberOfBalls = 0
+	wickets = 0
+	score = 0
+	ball_score = 0
+	overs = 0
+	b = 0                                  # Ball in over
+	
 
-batsman_1 = batsmanOrder[0]             # On-Strike Batsman
-batsman_2 = batsmanOrder[1]             # Off-Strike Batsman
-bowler = bowlerOrder[0].rstrip("\n")    # First Bowler
-bowler_index = 0                        # Index of next bowler
-batting_index = 1                       # Index of next batsman
+	batsman_1 = batsmanOrder[0].rstrip("\n")             # On-Strike Batsman
+	batsman_2 = batsmanOrder[1].rstrip("\n")             # Off-Strike Batsman
+	bowler = bowlerOrder[0].rstrip("\n")    # First Bowler
+	bowler_index = 0                        # Index of next bowler
+	batting_index = 1                       # Index of next batsman
 
-# Initial wicket probabilities
-wicket_prob = {(batsman_1, bowler): 1 - clustering_prob[(
-    batsman_1, bowler)]["W"], (batsman_2, bowler): 1 - clustering_prob[(batsman_2, bowler)]["W"]}
+	# Initial wicket probabilities
+	wicket_prob = {(batsman_1, bowler): 1 - clustering_prob[(
+    	batsman_1, bowler)]["W"], (batsman_2, bowler): 1 - clustering_prob[(batsman_2, bowler)]["W"]}
 
-while(wickets <= 10 and numberOfBalls <= 120):
-    b += 1
-    numberOfBalls += 1
-    # Updating wicket probability
-    if (batsman_1, bowler) not in wicket_prob:
-        wicket_prob[(batsman_1, bowler)] = 1 - clustering_prob[(batsman_1, bowler)]["W"]
-    else:
-        tmp = wicket_prob[(batsman_1, bowler)]
-        wicket_prob[(batsman_1, bowler)] = tmp * (1 - clustering_prob[(batsman_1, bowler)]["W"])
-    if(wicket_prob[(batsman_1, bowler)] < 0.7):
-        wickets += 1
-        batting_index += 1
-        batsman_1 = batsmanOrder[batting_index]
-        ball_score = 0
-        print("Wicket:", batsman_1)
-    else:
-        r = random.uniform(0.1, 0.9999)  
-        ball_score = CalculateRuns(r, batsman_1, bowler, cumulative_prob)
+	while(wickets <= 10 and numberOfBalls < 120):
+    		b += 1
+    		numberOfBalls += 1
+    		# Updating wicket probability
+    		if (batsman_1, bowler) not in wicket_prob:
+        		wicket_prob[(batsman_1, bowler)] = 1 - clustering_prob[(batsman_1, bowler)]["W"]
+    		else:
+        		tmp = wicket_prob[(batsman_1, bowler)]
+        		wicket_prob[(batsman_1, bowler)] = tmp * (1 - clustering_prob[(batsman_1, bowler)]["W"])
+    		if(wicket_prob[(batsman_1, bowler)] < 0.7):
+        		print("%d.%d\t%s\t%s\tW" % (overs,b,batsman_1,bowler))
+        		
+        		wickets += 1
+        		batting_index += 1
+        		batsman_1 = batsmanOrder[batting_index].rstrip("\n")
+        		ball_score = 0
+    		else:
+        		r = random.uniform(0.1, 0.9999)  
+        		ball_score = CalculateRuns(r, batsman_1, bowler, cumulative_prob)
+        		print("%d.%d\t%s\t%s\t%d" % (overs,b,batsman_1,bowler,ball_score))
+        		
+    		score += ball_score
 
-    score += ball_score
-    print("Score : %d/%d" % (score, wickets))
+    		if(ball_score%2):
+        		batsman_1, batsman_2 = batsman_2, batsman_1
 
-    # End of Over    
-    if(b == 7 and numberOfBalls<=120):
-        batsman_1, batsman_2 = batsman_2, batsman_1
-        bowler_index += 1
-        bowler = bowlerOrder[bowler_index].rstrip("\n")
-        b = 1
-        overs +=1
+    		# End of Over    
+    		if(b == 6 and numberOfBalls<120):
+        		batsman_1, batsman_2 = batsman_2, batsman_1
+        		bowler_index += 1
+        		bowler = bowlerOrder[bowler_index].rstrip("\n")
+        		b = 0
+        		overs +=1
+	print("Target =", score+1)	
+	return score
+
+def Innings2(Target):
+	'''batsmanOrder = ["Q de Kock","V Kohli","AB de Villiers","CJ Anderson","Mandeep Singh","C de Grandhomme","P Negi","Washington Sundar","UT Yadav","Mohammed Siraj","YS Chahal"]
+	bowlerOrder = ["SR Watson","MS Dhoni","AT Rayudu","SK Raina","SW Billings","RA Jadeja","SW Billings","DJ Bravo","Harbhajan Singh","MS Dhoni","SR Watson","DJ Bravo","Harbhajan Singh","AT Rayudu","DL Chahar","SN Thakur","AT Rayudu","Imran Tahir","MS Dhoni","SW Billings"]'''
+
+	bowlerOrder = []
+	batsmanOrder = []
+
+	data = open("Team2bats.csv", "r")
+	for line in data.readlines():
+    		batsmanOrder.append(line)
+
+	data = open("Team1bowl.csv", "r")
+	for line in data.readlines():
+    		bowlerOrder.append(line)
+
+	cumulative_prob = CP()
+	clustering_prob = getKMeansClusters()
+
+	numberOfBalls = 0
+	wickets = 0
+	score = 0
+	ball_score = 0
+	overs = 0
+	b = 0                                   # Ball in over
+	
+
+	batsman_1 = batsmanOrder[0].rstrip("\n")             # On-Strike Batsman
+	batsman_2 = batsmanOrder[1].rstrip("\n")             # Off-Strike Batsman
+	bowler = bowlerOrder[0].rstrip("\n")    # First Bowler
+	bowler_index = 0                        # Index of next bowler
+	batting_index = 1                       # Index of next batsman
+
+	# Initial wicket probabilities
+	wicket_prob = {(batsman_1, bowler): 1 - clustering_prob[(
+    	batsman_1, bowler)]["W"], (batsman_2, bowler): 1 - clustering_prob[(batsman_2, bowler)]["W"]}
+
+	while(wickets <= 10 and numberOfBalls < 120 and score<=Target):
+    		b += 1
+    		numberOfBalls += 1
+    		# Updating wicket probability
+    		if (batsman_1, bowler) not in wicket_prob:
+        		wicket_prob[(batsman_1, bowler)] = 1 - clustering_prob[(batsman_1, bowler)]["W"]
+    		else:
+        		tmp = wicket_prob[(batsman_1, bowler)]
+        		wicket_prob[(batsman_1, bowler)] = tmp * (1 - clustering_prob[(batsman_1, bowler)]["W"])
+    		if(wicket_prob[(batsman_1, bowler)] < 0.7):
+        		print("%d.%d\t%s\t%s\tW" % (overs,b,batsman_1,bowler))
+        		
+        		wickets += 1
+        		batting_index += 1
+        		batsman_1 = batsmanOrder[batting_index].rstrip("\n")
+        		ball_score = 0
+    		else:
+        		r = random.uniform(0.1, 0.9999)  
+        		ball_score = CalculateRuns(r, batsman_1, bowler, cumulative_prob)
+        		print("%d.%d\t%s\t%s\t%d" % (overs,b,batsman_1,bowler,ball_score))
+        		
+
+    		score += ball_score
+
+    		if(ball_score%2):
+        		batsman_1, batsman_2 = batsman_2, batsman_1
+
+    		# End of Over    
+    		if(b == 6 and numberOfBalls<120):
+        		batsman_1, batsman_2 = batsman_2, batsman_1
+        		bowler_index += 1
+        		bowler = bowlerOrder[bowler_index].rstrip("\n")
+        		b = 0
+        		overs +=1
+	print("Final Score :",score)
+	if(score == Target):
+    		print("Tie")
+	elif(Target > score):
+    		print("Team 1 wins by %d runs"%(Target-score))
+	elif(Target < score):
+		print("Team 2 wins by %d wickets"%(10-wickets))
+
+print("First Innings")
+Target = Innings1()
+print("Second Innings")
+Innings2(Target)
